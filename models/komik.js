30350053c85ef8ea8ad4942fb3412615ccc546bf
NurulNajatMony <nurulmony505@gmail.com>
2026-08-19 15:00:51 +0700

    feat: inisialisasi rest api perpustakaan komik dan autentikasi jwt

---
+++ models/komik.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        judul: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        sinopsis: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        tahun_terbit: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        penulis_Id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }, {
+        tableName: "komik",
+        timestamps: true
+    });
+    Komik.associate = (models) => {
+        Komik.belongsTo(models.Penulis, {
+            foreignKey: "penulis_Id",
+            as: "penulis"
+        });
+        Komik.belongsToMany(models.Genre, {
+            through: "komik_Genre",
+            foreignKey: "komik_Id",
+            otherKey: "genre_Id",
+            as: "genre"
+        });
+    };
+    return Komik;
+};
